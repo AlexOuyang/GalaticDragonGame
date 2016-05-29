56526bcf225e69b51ad9bdd45a25e7fcdd6a1d61
AlexOuyang <ouyangchenxing@hotmail.com>
2016-05-29 15:08:07 -0700

    added external libraris

--- Core/BezierCurve/build/BezierCurve.build/Debug/BezierCurve.build/Objects-normal/x86_64/shader.d
+++ Core/BezierCurve/build/BezierCurve.build/Debug/BezierCurve.build/Objects-normal/x86_64/shader.d
@@ -1,4 +1,4 @@
 dependencies: \
   /Users/chenxingouyang/Documents/Github/Ouyang-Alex/Project4/BezierCurve/BezierCurve/shader.cpp \
   /Users/chenxingouyang/Documents/Github/Ouyang-Alex/Project4/BezierCurve/BezierCurve/shader.h \
-  /Developer/glew-1.13.0/include/GL/glew.h
+  ../../lib/glew-1.13.0/include/GL/glew.h
